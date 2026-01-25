7df29ed723c7aca02ef8b0dae747d02dd108bc56
yirween <s5020@outlook.com>
2026-01-25 09:51:07 +0800

    修

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
-// import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
 
-// export async function GET(context) {
-//   return rss({
-//     title: 'Astro Learner | Blog',
-//     description: 'My journey learning Astro',
-//     site: context.site,
-//     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-//     customData: `<language>cn-zh</language>`,
-//   });
-// }
+export async function GET(context) {
+  const postImportResult = import.meta.glob('./posts/**/*.md', { eager: true });
+  const posts = Object.values(postImportResult);
+
+  return rss({
+    title: 'Yirween Blog',
+    description: 'Yirween 的个人博客',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.frontmatter.title,
+      pubDate: post.frontmatter.pubDate,
+      description: post.frontmatter.description,
+      link: post.url,
+    })),
+    customData: `<language>zh-cn</language>`,
+  });
+}
